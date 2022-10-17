f124fa0373dabb14a38ddb58c8a4c8bf22ded653
Abbasa5251 <abbasa5251@hotmail.com>
2022-10-17 16:10:40 +0530

    added working contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,11 +9,7 @@ function Contact() {
 		number: "",
 		message: "",
 	});
-	const [status, setStatus] = useState({
-		error: false,
-		success: false,
-		message: "",
-	});
+	const [status, setStatus] = useState({});
 
 	const handleInput = (e) => {
 		const { name, value } = e.target;
@@ -24,11 +20,7 @@ function Contact() {
 	};
 
 	const handleClose = (e) => {
-		setStatus({
-			success: false,
-			error: false,
-			message: "",
-		});
+		setStatus({});
 	};
 
 	const validatePhoneNumber = (str_number) => {
@@ -43,70 +35,43 @@ function Contact() {
 	};
 
 	const handleSubmit = (e) => {
-		if (data.firstName.trim().length == 0) {
-			setStatus({
-				success: false,
-				error: true,
-				message: "Please enter first name",
-			});
-			return;
-		}
-		if (data.lastName.trim().length == 0) {
-			setStatus({
-				success: false,
-				error: true,
-				message: "Please enter last name",
-			});
-			return;
-		}
-		if (data.email.trim().length == 0) {
-			setStatus({
-				success: false,
-				error: true,
-				message: "Please enter email",
-			});
-			return;
-		}
-		if (data.message.trim().length == 0) {
-			setStatus({
-				success: false,
-				message: "Please enter message",
-				error: true,
-			});
-			return;
-		}
+		e.preventDefault();
 		if (data.number.trim().length != 0) {
-			// if the user enters number
 			if (!validatePhoneNumber(data.number)) {
-				setStatus({
-					success: false,
-					message: "Please enter valid phone number",
-					error: true,
-				});
+				alert("Please enter a valid phone number");
 				return;
 			}
 		}
 		if (!validateEmail(data.email)) {
-			setStatus({
-				success: false,
-				message: "Please enter valid email",
-				error: true,
-			});
+			alert("Please enter a valid email address");
 			return;
 		}
-		// Submit form
-		console.log("Form Submitted");
+		fetch(`${import.meta.env.VITE_BASE_URL}/contact`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setStatus({
+					message: data.message,
+					status: "success",
+				});
+			})
+			.catch((error) => {
+				setStatus({
+					message: "There was an error submitting form",
+					status: "error",
+				});
+			});
 		setData({
 			firstName: "",
 			lastName: "",
 			email: "",
 			number: "",
 			message: "",
-		});
-		setStatus({
-			error: false,
-			success: true,
-			message: "Form has been submitted successfully",
 		});
 	};
 
@@ -127,189 +92,166 @@ function Contact() {
 					</p>
 				</div>
 				<div className="lg:w-1/2 md:w-2/3 mx-auto">
-					<div className="flex flex-wrap -m-2">
-						<div className="p-2 w-1/2">
-							<div className="relative">
-								<label
-									for="firstName"
-									className="leading-7 text-sm text-gray-400"
-								>
-									First Name
-									<span className="text-xs text-gray-500">
-										*
-									</span>
-								</label>
-								<input
-									type="text"
-									id="firstName"
-									name="firstName"
-									value={data.firstName}
-									onChange={handleInput}
-									className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-								/>
+					<form onSubmit={handleSubmit}>
+						<div className="flex flex-wrap -m-2">
+							<div className="p-2 w-1/2">
+								<div className="relative">
+									<label
+										htmlFor="firstName"
+										className="leading-7 text-sm text-gray-400"
+									>
+										First Name
+										<span className="text-xs text-gray-500">
+											*
+										</span>
+									</label>
+									<input
+										type="text"
+										id="firstName"
+										name="firstName"
+										required={true}
+										value={data.firstName}
+										onChange={handleInput}
+										className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									/>
+								</div>
 							</div>
-						</div>
-						<div className="p-2 w-1/2">
-							<div className="relative">
-								<label
-									for="lastName"
-									className="leading-7 text-sm text-gray-400"
-								>
-									Last Name
-									<span className="text-xs text-gray-500">
-										*
-									</span>
-								</label>
-								<input
-									type="text"
-									id="lastName"
-									name="lastName"
-									value={data.lastName}
-									onChange={handleInput}
-									className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-								/>
+							<div className="p-2 w-1/2">
+								<div className="relative">
+									<label
+										htmlFor="lastName"
+										className="leading-7 text-sm text-gray-400"
+									>
+										Last Name
+										<span className="text-xs text-gray-500">
+											*
+										</span>
+									</label>
+									<input
+										type="text"
+										id="lastName"
+										name="lastName"
+										required={true}
+										value={data.lastName}
+										onChange={handleInput}
+										className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									/>
+								</div>
 							</div>
-						</div>
-						<div className="p-2 w-1/2">
-							<div className="relative">
-								<label
-									for="email"
-									className="leading-7 text-sm text-gray-400"
-								>
-									Email
-									<span className="text-xs text-gray-500">
-										*
-									</span>
-								</label>
-								<input
-									type="email"
-									id="email"
-									name="email"
-									value={data.email}
-									onChange={handleInput}
-									className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-								/>
+							<div className="p-2 w-1/2">
+								<div className="relative">
+									<label
+										htmlFor="email"
+										className="leading-7 text-sm text-gray-400"
+									>
+										Email
+										<span className="text-xs text-gray-500">
+											*
+										</span>
+									</label>
+									<input
+										type="email"
+										id="email"
+										name="email"
+										required={true}
+										value={data.email}
+										onChange={handleInput}
+										className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									/>
+								</div>
 							</div>
-						</div>
-						<div className="p-2 w-1/2">
-							<div className="relative">
-								<label
-									for="number"
-									className="leading-7 text-sm text-gray-400"
-								>
-									Phone Number
-								</label>
-								<input
-									type="text"
-									id="number"
-									name="number"
-									value={data.number}
-									onChange={handleInput}
-									className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-								/>
+							<div className="p-2 w-1/2">
+								<div className="relative">
+									<label
+										htmlFor="number"
+										className="leading-7 text-sm text-gray-400"
+									>
+										Phone Number
+									</label>
+									<input
+										type="text"
+										id="number"
+										name="number"
+										value={data.number}
+										onChange={handleInput}
+										className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+									/>
+								</div>
 							</div>
-						</div>
-						<div className="p-2 w-full">
-							<div className="relative">
-								<label
-									for="message"
-									className="leading-7 text-sm text-gray-400"
-								>
-									Message
-									<span className="text-xs text-gray-500">
-										*
-									</span>
-								</label>
-								<textarea
-									id="message"
-									name="message"
-									value={data.message}
-									onChange={handleInput}
-									className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-								></textarea>
+							<div className="p-2 w-full">
+								<div className="relative">
+									<label
+										htmlFor="message"
+										className="leading-7 text-sm text-gray-400"
+									>
+										Message
+										<span className="text-xs text-gray-500">
+											*
+										</span>
+									</label>
+									<textarea
+										id="message"
+										name="message"
+										required={true}
+										value={data.message}
+										onChange={handleInput}
+										className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-green-500 focus:bg-gray-900 focus:ring-2 focus:ring-green-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+									></textarea>
+								</div>
 							</div>
-						</div>
-						{status.error && (
-							<div className="container px-4 mx-auto">
-								<div className="mx-auto p-6 bg-gray-900 border border-gray-700 rounded-md shadow-dashboard">
-									<div className="flex flex-wrap justify-center -m-2">
-										<div className="flex-1 p-2">
-											<div className="flex flex-wrap">
-												<div className="flex-1 p-1">
-													<p className="font-medium text-sm text-orange-600">
-														{status.message}
-													</p>
+							{"status" in status && (
+								<div className="container px-4 mx-auto">
+									<div className="mx-auto p-6 bg-gray-900 border border-gray-700 rounded-md">
+										<div className="flex flex-wrap justify-center -m-2">
+											<div className="flex-1 p-2">
+												<div className="flex flex-wrap">
+													<div className="flex-1 p-1">
+														<p
+															className={`font-medium text-sm ${
+																status.status ==
+																"success"
+																	? "text-green-500"
+																	: "text-red-500"
+															}`}
+														>
+															{status.message}
+														</p>
+													</div>
 												</div>
 											</div>
-										</div>
-										<div className="w-auto p-2">
-											<span
-												className="cursor-pointer"
-												onClick={handleClose}
-											>
-												<svg
-													width="16"
-													height="16"
-													viewbox="0 0 16 16"
-													fill="none"
-													xmlns="http://www.w3.org/2000/svg"
+											<div className="w-auto p-2">
+												<span
+													className="cursor-pointer"
+													onClick={handleClose}
 												>
-													<path
-														d="M8.93999 8L13.14 3.80667C13.2655 3.68113 13.3361 3.51087 13.3361 3.33333C13.3361 3.1558 13.2655 2.98554 13.14 2.86C13.0145 2.73446 12.8442 2.66394 12.6667 2.66394C12.4891 2.66394 12.3189 2.73446 12.1933 2.86L8 7.06L3.80666 2.86C3.68113 2.73446 3.51086 2.66394 3.33333 2.66394C3.15579 2.66394 2.98553 2.73446 2.85999 2.86C2.73446 2.98554 2.66393 3.1558 2.66393 3.33333C2.66393 3.51087 2.73446 3.68113 2.85999 3.80667L7.06 8L2.85999 12.1933C2.79751 12.2553 2.74791 12.329 2.71407 12.4103C2.68022 12.4915 2.6628 12.5787 2.6628 12.6667C2.6628 12.7547 2.68022 12.8418 2.71407 12.9231C2.74791 13.0043 2.79751 13.078 2.85999 13.14C2.92197 13.2025 2.9957 13.2521 3.07694 13.2859C3.15818 13.3198 3.24532 13.3372 3.33333 13.3372C3.42134 13.3372 3.50847 13.3198 3.58971 13.2859C3.67095 13.2521 3.74469 13.2025 3.80666 13.14L8 8.94L12.1933 13.14C12.2553 13.2025 12.329 13.2521 12.4103 13.2859C12.4915 13.3198 12.5787 13.3372 12.6667 13.3372C12.7547 13.3372 12.8418 13.3198 12.923 13.2859C13.0043 13.2521 13.078 13.2025 13.14 13.14C13.2025 13.078 13.2521 13.0043 13.2859 12.9231C13.3198 12.8418 13.3372 12.7547 13.3372 12.6667C13.3372 12.5787 13.3198 12.4915 13.2859 12.4103C13.2521 12.329 13.2025 12.2553 13.14 12.1933L8.93999 8Z"
-														fill="#8896AB"
-													></path>
-												</svg>
-											</span>
+													<svg
+														width="16"
+														height="16"
+														viewBox="0 0 16 16"
+														fill="none"
+														xmlns="http://www.w3.org/2000/svg"
+													>
+														<path
+															d="M8.93999 8L13.14 3.80667C13.2655 3.68113 13.3361 3.51087 13.3361 3.33333C13.3361 3.1558 13.2655 2.98554 13.14 2.86C13.0145 2.73446 12.8442 2.66394 12.6667 2.66394C12.4891 2.66394 12.3189 2.73446 12.1933 2.86L8 7.06L3.80666 2.86C3.68113 2.73446 3.51086 2.66394 3.33333 2.66394C3.15579 2.66394 2.98553 2.73446 2.85999 2.86C2.73446 2.98554 2.66393 3.1558 2.66393 3.33333C2.66393 3.51087 2.73446 3.68113 2.85999 3.80667L7.06 8L2.85999 12.1933C2.79751 12.2553 2.74791 12.329 2.71407 12.4103C2.68022 12.4915 2.6628 12.5787 2.6628 12.6667C2.6628 12.7547 2.68022 12.8418 2.71407 12.9231C2.74791 13.0043 2.79751 13.078 2.85999 13.14C2.92197 13.2025 2.9957 13.2521 3.07694 13.2859C3.15818 13.3198 3.24532 13.3372 3.33333 13.3372C3.42134 13.3372 3.50847 13.3198 3.58971 13.2859C3.67095 13.2521 3.74469 13.2025 3.80666 13.14L8 8.94L12.1933 13.14C12.2553 13.2025 12.329 13.2521 12.4103 13.2859C12.4915 13.3198 12.5787 13.3372 12.6667 13.3372C12.7547 13.3372 12.8418 13.3198 12.923 13.2859C13.0043 13.2521 13.078 13.2025 13.14 13.14C13.2025 13.078 13.2521 13.0043 13.2859 12.9231C13.3198 12.8418 13.3372 12.7547 13.3372 12.6667C13.3372 12.5787 13.3198 12.4915 13.2859 12.4103C13.2521 12.329 13.2025 12.2553 13.14 12.1933L8.93999 8Z"
+															fill="#8896AB"
+														></path>
+													</svg>
+												</span>
+											</div>
 										</div>
 									</div>
 								</div>
+							)}
+							<div className="p-2 w-full">
+								<button
+									type="submit"
+									className="flex mx-auto text-green-50 bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg"
+								>
+									Submit
+								</button>
 							</div>
-						)}
-						{status.success && (
-							<div className="container px-4 mx-auto">
-								<div className="mx-auto p-6 bg-gray-900 border border-gray-700 rounded-md">
-									<div className="flex flex-wrap justify-center -m-2">
-										<div className="flex-1 p-2">
-											<div className="flex flex-wrap">
-												<div className="flex-1 p-1">
-													<p className="font-medium text-sm text-green-500">
-														{status.message}
-													</p>
-												</div>
-											</div>
-										</div>
-										<div className="w-auto p-2">
-											<span
-												className="cursor-pointer"
-												onClick={handleClose}
-											>
-												<svg
-													width="16"
-													height="16"
-													viewbox="0 0 16 16"
-													fill="none"
-													xmlns="http://www.w3.org/2000/svg"
-												>
-													<path
-														d="M8.93999 8L13.14 3.80667C13.2655 3.68113 13.3361 3.51087 13.3361 3.33333C13.3361 3.1558 13.2655 2.98554 13.14 2.86C13.0145 2.73446 12.8442 2.66394 12.6667 2.66394C12.4891 2.66394 12.3189 2.73446 12.1933 2.86L8 7.06L3.80666 2.86C3.68113 2.73446 3.51086 2.66394 3.33333 2.66394C3.15579 2.66394 2.98553 2.73446 2.85999 2.86C2.73446 2.98554 2.66393 3.1558 2.66393 3.33333C2.66393 3.51087 2.73446 3.68113 2.85999 3.80667L7.06 8L2.85999 12.1933C2.79751 12.2553 2.74791 12.329 2.71407 12.4103C2.68022 12.4915 2.6628 12.5787 2.6628 12.6667C2.6628 12.7547 2.68022 12.8418 2.71407 12.9231C2.74791 13.0043 2.79751 13.078 2.85999 13.14C2.92197 13.2025 2.9957 13.2521 3.07694 13.2859C3.15818 13.3198 3.24532 13.3372 3.33333 13.3372C3.42134 13.3372 3.50847 13.3198 3.58971 13.2859C3.67095 13.2521 3.74469 13.2025 3.80666 13.14L8 8.94L12.1933 13.14C12.2553 13.2025 12.329 13.2521 12.4103 13.2859C12.4915 13.3198 12.5787 13.3372 12.6667 13.3372C12.7547 13.3372 12.8418 13.3198 12.923 13.2859C13.0043 13.2521 13.078 13.2025 13.14 13.14C13.2025 13.078 13.2521 13.0043 13.2859 12.9231C13.3198 12.8418 13.3372 12.7547 13.3372 12.6667C13.3372 12.5787 13.3198 12.4915 13.2859 12.4103C13.2521 12.329 13.2025 12.2553 13.14 12.1933L8.93999 8Z"
-														fill="#8896AB"
-													></path>
-												</svg>
-											</span>
-										</div>
-									</div>
-								</div>
-							</div>
-						)}
-						<div className="p-2 w-full">
-							<button
-								className="flex mx-auto text-green-50 bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg"
-								onClick={handleSubmit}
-							>
-								Submit
-							</button>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		</section>
